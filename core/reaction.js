658bdbb447bb5b6955162eb6c0d92c140e044b65
angxuejian <xuejian.ang@gmail.com>
2026-01-10 12:38:26 +0800

    feat: add "v-if"、“v-for”、“v-model” and improve "defineReaction" and add "defineReactionSet"

--- core/reaction.js
+++ core/reaction.js
@@ -1,38 +1,111 @@
-function defindReaction(obj) {
+function defineReaction(obj) {
   if (typeof obj !== "object" || obj === null) return obj;
+  else if (Array.isArray(obj)) {
+    definePropertyArr(obj);
 
-  Object.keys(obj).forEach((key) => {
-    defindReaction(obj[key]);
-
-    const _key = `_${key}`;
-
-    Object.defineProperty(obj, `_${key}`, {
-      value: obj[key],
-      writable: true,
-      enumerable: false,
-      configurable: true,
+    obj.forEach((item) => defineReaction(item));
+  } else {
+    Object.keys(obj).forEach((key) => {
+      defineReaction(obj[key]);
+      definePropertyObj(obj, key)
     });
-
-    Object.defineProperty(obj, key, {
-      get() {
-        console.log(`读取: ${_key}`);
-        return this[_key];
-      },
-      set(newVal) {
-        if (newVal === this[_key]) return;
-
-        console.log(`赋值: ${_key}`);
-
-        this[_key] = newVal;
-
-        defindReaction(newVal);
-
-        if (typeof update === "function") {
-          update();
-        }
-      },
-    });
-  });
-  
+  }
   return obj;
+}
+function definePropertyObj(obj, key) {
+  const _key = `_${key}`;
+
+  Object.defineProperty(obj, `_${key}`, {
+    value: obj[key],
+    writable: true,
+    enumerable: false,
+    configurable: true,
+  });
+
+  Object.defineProperty(obj, key, {
+    get() {
+      console.log(`读取: ${_key}`);
+      return this[_key];
+    },
+    set(newVal) {
+      if (newVal === this[_key]) return;
+
+      console.log(`赋值: ${_key}`);
+
+      this[_key] = newVal;
+
+      defineReaction(newVal);
+
+      defineUpdate()
+    },
+  });
+}
+
+function definePropertyArr(arr) {
+  const arrayPrototype = Array.prototype;
+  const newArrayPrototype = Object.create(arrayPrototype);
+  const resetMethods = [
+    "push",
+    "pop",
+    "shift",
+    "unshift",
+    "splice",
+    "sort",
+    "reverse",
+  ];
+
+  resetMethods.forEach((method) => {
+    newArrayPrototype[method] = function (...args) {
+      console.log(`执行: ${method}; ${args}`);
+      const result = arrayPrototype[method].apply(this, args);
+
+      let inserted = null;
+
+      switch (method) {
+        case "push":
+        case "unshift":
+          inserted = args;
+          break;
+        case "splice":
+          // splice(start, deleteCount, ...items)
+          inserted = args.slice(2);
+          break;
+      }
+
+      // 如果有新增数据，就再次循环绑定响应式
+      if (inserted) {
+        inserted.forEach((item) => defineReaction(item));
+      }
+
+      defineUpdate()
+
+      return result;
+    };
+  });
+
+  arr.__proto__ = newArrayPrototype;
+}
+
+function defineReactionSet(target, key, value) {
+  // 数组
+  if (Array.isArray(target) && typeof key === "number") {
+    target.splice(key, 1, value);
+    return;
+  }
+
+  // 已有属性 → 直接走 setter
+  if (key in target) {
+    target[key] = value;
+    return;
+  }
+
+  // 新增属性
+  definePropertyObj(target, key)
+  defineUpdate()
+}
+
+function defineUpdate() {
+  if (typeof update === "function") {
+    update();
+  }
 }
--- views/home.js
+++ views/home.js
@@ -1,9 +1,13 @@
 function homeComponent() {
-  const data = defindReaction({
+  const data = defineReaction({
     user: {
       name: "yuhua",
     },
+    isShow: false,
+    inputVal: 'v-model: value'
   });
+
+  const list = defineReaction([0])
 
   return function render() {
     return h("div", null, [
@@ -12,11 +16,23 @@ function homeComponent() {
         "button",
         {
           onclick: () => {
-            data.user.name += "1";
+            // list.push(list.length)
+            // data.isShow = !data.isShow
+            defineReactionSet(list, 0, list[0] += 1)
           },
         },
         "add"
       ),
+      h('div', null, [
+        (data.isShow ? h('p', null, 'v-if: show') : '')
+      ]),
+      h('ul', null, [
+        ...(list.map((_, index) => h('li', null, `v-for: ${_}`)))
+      ]),
+      h('input', { value: data.inputVal, oninput: (event) => {
+        data.inputVal = event.target.value
+        console.log(inputVal)
+      } })
     ]);
   };
 }
